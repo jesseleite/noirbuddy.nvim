31cf2c7d8dc3dda5bf2b91b9c0796ed3677e0527
Jesse Leite <jesseleite@gmail.com>
2025-10-07 16:20:44 -0400

    Make treesitter-context separator color more subtle (when configured).

--- lua/noirbuddy/plugins/treesitter-context.lua
+++ lua/noirbuddy/plugins/treesitter-context.lua
@@ -2,3 +2,4 @@ local Group = require('colorbuddy').Group
 local colors = require('colorbuddy').colors
 
 Group.new('TreesitterContextLineNumber', colors.noir_7)
+Group.new('TreesitterContextSeparator', colors.noir_8)
